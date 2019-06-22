82c3969725b39cb1e33f32553c24e74d92a3f31e
Luis Preza <luis@rtwebs.net>
2019-06-22 15:23:53 -0500

    a soultion for wordpress cors

--- backend-laravel/app/Http/Middleware/CorsMiddleware.php
+++ backend-laravel/app/Http/Middleware/CorsMiddleware.php
@@ -15,9 +15,21 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-          ->header('Access-Control-Allow-Origin' , env('FRONTEND_ENDPOINT', 'http://localhost:8080'))
-          ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-          ->header('Access-Control-Allow-Headers', '*');
+
+      $allowedOrigins = [env('FRONTEND_ENDPOINT', 'http://localhost:8080'), env('WORDPRESS_ENDPOINT', 'http://localhost'), env('EXTRA_ENDPOINT', 'http://127.0.0.1')];
+
+      if($request->server('HTTP_ORIGIN')){
+        if (in_array($request->server('HTTP_ORIGIN'), $allowedOrigins)) {
+            return $next($request)
+                ->header('Access-Control-Allow-Origin', $request->server('HTTP_ORIGIN'))
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+                ->header('Access-Control-Allow-Headers', '*');
+        }
+      }
+
+
+      return $next($request);
+
     }
 }
